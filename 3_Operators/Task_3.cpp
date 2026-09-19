03bddfa675ad915c1bb3fff6ff913e74c9068745
Muhammad Hassan <mhassanawan66521@gmail.com>
2026-09-19 16:59:28 +0500

    Operators added

---
+++ 3_Operators/Task_3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a = 10;
+	int b = 5;
+
+	cout << (a > b);
+	cout<<"\n";
+	cout << (a == b);
+	return 0;
+}
+
---
+++ 3_Operators/Task_4.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int x = 5;
+
+	cout << x++ << endl;
+	cout << ++x;
+	cout<<"\n";
+	int y = 10;
+	cout << 5 + 3 * 2;
+	cout<<"\n"<<(5+3) * 2;
+	cout<<"\n";
+	int a = 5;
+	cout<<(a > 0 && a < 10);
+	cout<<endl;
+	int d = 5;
+	cout << d++ + ++d;
+	return 0;
+}
